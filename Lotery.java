b9c1441b7201b5232d68c4160a7f090dc9c64d03
Diego Hauptman <diegohauptman@gmail.com>
2018-09-18 20:40:36 +0200

    Update Lotery.java

--- Lotery.java
+++ Lotery.java
@@ -7,11 +7,7 @@ import java.util.Collections;
  */
 public class Lotery {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         ArrayList<Integer> sixRandomNumbersList = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             int randomNumber = (int) (Math.random() * 50) + 1;
