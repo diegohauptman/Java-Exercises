d1133d984f3ea72e6af22f7ce21550acb150374c
Diego Hauptman <diegohauptman@gmail.com>
2018-09-18 20:39:23 +0200

    Add files via upload

---
+++ Lotery.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lotery;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Six different random numbers between 1 to 49 ascending order.
+ * @author D. Hauptman
+ */
+public class Lotery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> sixRandomNumbersList = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int randomNumber = (int) (Math.random() * 50) + 1;
+            sixRandomNumbersList.add(randomNumber);
+        }
+        Collections.sort(sixRandomNumbersList);
+
+        for (int i : sixRandomNumbersList) {
+            System.out.println(i);
+        }
+        System.out.println(sixRandomNumbersList);
+    }
+}
